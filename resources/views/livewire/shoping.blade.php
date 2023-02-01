bb28dbb46d0b2398712e5d412f57b32528a02cc9
SandySNZ2006 <sandysnz99@gmail.com>
2023-02-01 23:19:40 +0700

    Pertama

---
+++ resources/views/livewire/shoping.blade.php
@@ -0,0 +1,91 @@
+<div>    
+    <div class="container mb-3">
+        <div class="row">
+            <div class="col-3">
+                <div class="card text-white mb-3">
+                    <div class="card-header bg-secondary">
+                        Keranjang
+                    </div>
+                    <div class="card-body bg-dark">
+                        
+                    </div>
+                </div>
+                <div class="card text-white">
+                    <div class="card-header bg-secondary">
+                        Belanjaan Saya
+                    </div>
+                    <div class="card-body bg-dark">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card">
+                    <div class="card-header text-white bg-secondary">
+                        Produk
+                    </div>
+                    <div class="card-body text-white bg-dark">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="card text-center h-100">
+                                    <img src="{{ asset('gambar/BuahNaga.jpg') }}" class="card-img-top" height="100rem" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-dark">Buah Naga</h5>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item text-dark">Rp.15.000 / Kg</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <button class="btn btn-danger">BELI</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card text-center h-100">
+                                    <img src="{{ asset('gambar/Apel.jpg') }}" class="card-img-top" height="100rem" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-dark">Apel</h5>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item text-dark">Rp.20.000 / Kg</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <button class="btn btn-danger">BELI</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card text-center h-100">
+                                    <img src="{{ asset('gambar/Pisang.jpg') }}" class="card-img-top" height="100rem" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-dark">Pisang</h5>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item text-dark">Rp. 10.000 / Kg</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <button class="btn btn-danger">BELI</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="card text-center h-100">
+                                    <img src="{{ asset('gambar/Nanas.jpg') }}" class="card-img-top" height="100rem" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-dark">Nanas</h5>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item text-dark">Rp. 5.000 / Buah</li>
+                                    </ul>
+                                    <div class="card-body">
+                                        <button class="btn btn-danger">BELI</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
